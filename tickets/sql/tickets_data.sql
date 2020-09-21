767f0a40c8ceb7204398cc847efefe8dbd125d88
HaEunji <ppippu12@gmail.com>
2020-09-22 00:24:55 +0900

    은지 - 공연데이터 sql 파일. 공연 등록 귀찮으시면 이거 돌려서 테스트 해보세요!

---
+++ tickets/sql/tickets_data.sql
@@ -0,0 +1,54 @@
+-- =================performance 입력 데이터======================
+Insert into TICKETS.PERFORMANCE (PER_NO,MEMBER_ID,CATEGORY_CODE,LOCATION_CODE,PER_TITLE,PER_DIRECTOR,PER_ACTOR,PER_ADDRESS,PER_CONTENT,PER_IMG_ORIGINAL_FILENAME,PER_IMG_RENAMED_FILENAME,DETAIL_IMG_ORIGINAL_FILENAME,DETAIL_IMG_RENAMED_FILENAME,PER_RATING,PER_DISPLAY,ADMIN_APPROVAL,PER_REGISTER_DATE) values (1,'company1','C5','L1','스폰지밥의 행복을 찾아서','대원미디어','스폰지밥','null 팝콘D스퀘어 대원뮤지엄','★ 다중이용시설 입장으로 QR코드 스캔 및 전 관람객 체온측정을 하고 있습니다.
+이에 입장시간이 다소 지연되더라고 양해 부탁드리겠습니다.
+
+★ 코로나19 예방으로 관람객들의 안전을 위하여 마스크 착용을 의무화 하고 있습니다.
+미착용시 입장이 제한됩니다. 모두가 안전하게 관람할 수 있도록 협조 부탁 드립니다.
+
+★ 주차 시 아이파크몰 해주차장 5층에 주차하시고 6층 대원뮤지엄으로 오시면 됩니다.
+
+★ 주차 무료 등록 대원뮤지엄 2시간 (티켓부스 직원에게 요청하시면 됩니다.)','스폰지밥.png','20200921_221140332_898.png','20006275-04.jpg','20200921_221140335_859.jpg',0,'N','N',to_date('20/09/21','RR/MM/DD'));
+Insert into TICKETS.PERFORMANCE (PER_NO,MEMBER_ID,CATEGORY_CODE,LOCATION_CODE,PER_TITLE,PER_DIRECTOR,PER_ACTOR,PER_ADDRESS,PER_CONTENT,PER_IMG_ORIGINAL_FILENAME,PER_IMG_RENAMED_FILENAME,DETAIL_IMG_ORIGINAL_FILENAME,DETAIL_IMG_RENAMED_FILENAME,PER_RATING,PER_DISPLAY,ADMIN_APPROVAL,PER_REGISTER_DATE) values (3,'company1','C3','L1','데이브레이크 ‘× ÷ SHOW : 몇 배의 즐거움을 함께 나누자’','민트페이퍼','데이브레이크','null 엠피엠지 2층 LOUNGE M.','- 각 회차별 1인당 2매까지 예매 가능합니다.
+- 본인 이외 티켓의 대리 수령 및 양도는 허용하지 않습니다.
+반드시 공연을 관람할 본인 명의의 아이디로 예매하여 주시기 바랍니다.
+- 예매 시 지정한 번호에 따라 순서대로 입장하게 됩니다. 빠른 번호를 예매하실수록 먼저 입장하실 수 있으며 원하는 좌석에 착석하시면 됩니다.
+- 본인 확인 절차를 위해 예매 시 현장 수령만 가능합니다. (티켓 배송 불가)
+- 예매는 평일 공연과 토요일 공연은 공연 전날 오후 5시까지,
+일요일 공연은 공연 전날 오전 11시까지 예매 가능합니다.
+예매 마감 후에는 취소, 환불이 불가능하니 이점 유의하시기 바랍니다.
+- 예매 시 결제수단으로 무통장 입금 선택 가능 기간은 아래와 같습니다.
+9월 25일(금) ~ 9월 27일(일) 공연 : 무통장 입금 불가
+10월 15일(목) ~ 10월17일(토) 공연 : 10월 8일(목) 자정까지
+10월 29일(목), 11월 1일(일) 공연 : 10월 22일(목) 자정까지
+- 각 은행사마다 입금 가능 시간이 상이하오니 미리 확인하시어 예매에 착오가 없도록 유의 바랍니다.
+- 예매 취소 시 예매처에서 정한 취소 수수료가 발생됩니다.
+
+* 코로나 바이러스의 대비 및 관객분들의 안전한 공연 관람을 위해 정부 방역 지침에 따라
+＇거리두기 좌석제＇로 운영 됩니다.
+* 본 공연은 철저한 방역 대비 아래 관객 50인 미만으로 제한되어 진행됩니다.
+* 9월 21일에 공시된 모든 회차의 티켓이 오픈됩니다. 추후 11월 공연은 10월 중 공지됩니다.','데이브레이크.png','20200921_221623679_5.png','데이브레이크 콘서트.jpg','20200921_221623681_347.jpg',0,'N','N',to_date('20/09/21','RR/MM/DD'));
+Insert into TICKETS.PERFORMANCE (PER_NO,MEMBER_ID,CATEGORY_CODE,LOCATION_CODE,PER_TITLE,PER_DIRECTOR,PER_ACTOR,PER_ADDRESS,PER_CONTENT,PER_IMG_ORIGINAL_FILENAME,PER_IMG_RENAMED_FILENAME,DETAIL_IMG_ORIGINAL_FILENAME,DETAIL_IMG_RENAMED_FILENAME,PER_RATING,PER_DISPLAY,ADMIN_APPROVAL,PER_REGISTER_DATE) values (4,'company1','C1','L1','뮤지컬 〈고스트〉','Matthew Warchus','주원, 아이비, 김우형','null 7층 디큐브아트센터','영화의 스펙터클 그대로 무대화한 뮤지컬 <고스트>, 7년 만에 다시 돌아오다
+영국 웨스트엔드에서 탄생한 뮤지컬 <고스트>는 지난 2013년 국내 초연되어 7개월간 23만 관객을 동원하며 큰 사랑을 받았다. 이 작품은 죽음을 초월한 두 남녀의 가슴 절절한 사랑 이야기를 최첨단 무대로 형상화하여, 영화를 방불케 하는 스펙터클로 큰 사랑을 받았다.
+하지만 뮤지컬 <고스트> 재공연은 쉽게 진행되지 못했다.
+무대 셋업 기간만 약 2개월, 최신 극장 시스템 없이는 설치가 불가능한 대형 메커니즘, 1,200석 이상의 극장에서 5개월 이상 공연되어야 하는 작품의 매머드급 규모로 손쉽게 추진할 수 없었다. 2020년 신시컴퍼니는 디큐브아트센터를 3년 대관하며 신시컴퍼니 자체 공연들의 일정을 조정하여, 드디어 7년 만에 뮤지컬 <고스트>를 다시 선보일 수 있게 되었다.','뮤지컬-고스트.gif','20200921_222141492_305.gif','뮤지컬-고스트설명.jpg','20200921_222141493_456.jpg',8,'N','N',to_date('20/09/21','RR/MM/DD'));
+Insert into TICKETS.PERFORMANCE (PER_NO,MEMBER_ID,CATEGORY_CODE,LOCATION_CODE,PER_TITLE,PER_DIRECTOR,PER_ACTOR,PER_ADDRESS,PER_CONTENT,PER_IMG_ORIGINAL_FILENAME,PER_IMG_RENAMED_FILENAME,DETAIL_IMG_ORIGINAL_FILENAME,DETAIL_IMG_RENAMED_FILENAME,PER_RATING,PER_DISPLAY,ADMIN_APPROVAL,PER_REGISTER_DATE) values (5,'company1','C2','L1','연극 〈쉬어매드니스〉','콘텐츠플래닝','임정균,이승진,김경호,임수형,이인규,최윤영,김번영,김유리','null 대학빌딩 지하1층 콘텐츠박스(KONTENTZ BOX)','안녕하세요. 연극 〈쉬어매드니스〉 입니다.
+임정균 배우의 개인사정으로 조호진 役 캐스팅이 변경되었습니다.
+관객 분들의 너른 양해 부탁드립니다.
+
+-9/23(수)8시 이승진-〉임정균
+-9/24(목)8시 이승진-〉임정균
+-9/25(금)8시 이승진-〉임정균
+-9/26(토)4시 임정균-〉이승진
+-9/26(토)7시 임정균-〉이승진
+-9/29(화)7시 임정균-〉이승진
+
+해당 회차의 경우 9월 22일(화) 오후 5시까지 인터파크 고객센터로
+연락주신 건에 한해 수수료 포함 전액 환불 가능합니다.
+다시 한 번 관객분들께 불편을 드리게 된 점 진심으로 사과의 말씀드립니다.
+감사합니다.','연극-쉬어매드니스.png','20200921_222527516_871.png','연극-쉬어매드니스 설명.jpg','20200921_222527518_732.jpg',15,'N','N',to_date('20/09/21','RR/MM/DD'));
+Insert into TICKETS.PERFORMANCE (PER_NO,MEMBER_ID,CATEGORY_CODE,LOCATION_CODE,PER_TITLE,PER_DIRECTOR,PER_ACTOR,PER_ADDRESS,PER_CONTENT,PER_IMG_ORIGINAL_FILENAME,PER_IMG_RENAMED_FILENAME,DETAIL_IMG_ORIGINAL_FILENAME,DETAIL_IMG_RENAMED_FILENAME,PER_RATING,PER_DISPLAY,ADMIN_APPROVAL,PER_REGISTER_DATE) values (6,'company1','C4','L1','〈쇼팽탄생 210주년〉 쇼팽으로 만나는 지브리 오케스트라','스톰프뮤직','정한빈,안두현','null 롯데월드몰 8층 롯데콘서트홀','스톰프뮤직은 관객 여러분들이 안전하고 쾌적하게 공연을 관람하시며 안심하고 공연장을 찾으실 수 있도록 공연장과 함께 아래와 같이 노력하고 있습니다.
+
+1. 공연장에서는 반복 청소와 방역을 실시하고, 공연장 입장 전에, 전 관객대상 체온 측정을 의무화하고 있습니다.
+2. 스톰프뮤직에서는 별도로 1회용 부직포 마스크 구비, 손소독제를 비치 등 관객분들이 위생적으로 공연을 관람하실 수 있도록 최선을 다하고 있습니다.
+3. 관객 여러분들께서는 마스크를 꼭 지참하시어 공연 관람 중 마스크 필수 착용 부탁드립니다.
+* 본 공연은 관객 여러분의 안전하고 위생적인 관람을 위해 1칸씩 띄워앉기를 시행합니다. 예매 시 참고 부탁드립니다.','클래식-쇼팽으로만나는지브리오케스트라.gif','20200921_222852632_668.gif','클래식-쇼팽으로만나는지브리오케스트라 설명.jpg','20200921_222852634_340.jpg',8,'N','N',to_date('20/09/21','RR/MM/DD'));
